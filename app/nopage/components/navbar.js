a9eb8f3915e238562d5174c5d4d5bd922d65838d
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-01-27 19:31:10 +0530

    start

---
+++ app/nopage/components/navbar.js
@@ -0,0 +1,35 @@
+"use client"
+
+import { TfiMenu } from "react-icons/tfi";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/logo.png"
+
+export default function navbar() {
+    return (
+        <>
+            <div className="flex flex-row flex-wrap justify-between border-4 p-2 border-red-800">
+                <div className="flex flex-row items-center justify-center border-4 border-red-800 ">
+                    <div className="mr-4">
+                        <TfiMenu className="h-6 w-6" />
+                    </div>
+                    <div className="">
+                        <Link className="flex flex-row" href="/">
+                            <Image
+                                src={logo}
+                                width={30}
+                                height={30}
+                            />
+                            <h1 className="text-2xl">dezynUI</h1>
+                        </Link>
+
+                    </div>
+
+                </div>
+                <div className="border-4 border-red-800 ">
+
+                </div>
+            </div>
+        </>
+    )
+}

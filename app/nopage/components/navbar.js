cf4da90710f7f081c9635159b056d2fcaad75466
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-01-27 21:21:40 +0530

    update

--- app/nopage/components/navbar.js
+++ app/nopage/components/navbar.js
@@ -11,7 +11,9 @@ export default function navbar() {
             <div className="flex flex-row flex-wrap justify-between border-4 p-2 border-red-800">
                 <div className="flex flex-row items-center justify-center border-4 border-red-800 ">
                     <div className="mr-4">
-                        <TfiMenu className="h-6 w-6" />
+                        <button>
+                            <TfiMenu className="h-6 w-6" />
+                        </button>
                     </div>
                     <div className="">
                         <Link className="flex flex-row" href="/">
@@ -20,14 +22,13 @@ export default function navbar() {
                                 width={30}
                                 height={30}
                             />
-                            <h1 className="text-2xl">dezynUI</h1>
+                            <h1 className="text-2xl font-bold">dezynUI</h1>
                         </Link>
-
                     </div>
 
                 </div>
                 <div className="border-4 border-red-800 ">
-
+                    
                 </div>
             </div>
         </>

4ab63b2ba61ef41d7d413ea102577665999d4edf
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-02-05 18:09:50 +0530

    hovered

--- app/nopage/components/navbar.js
+++ app/nopage/components/navbar.js
@@ -21,14 +21,23 @@ export default function navbar() {
 
     return (
         <>
-            <div className="flex flex-row flex-wrap justify-between py-4 p-2" style={{ backgroundColor: theme.theme1, color: theme.headingc }}>
+
+           <style jsx>
+            {`
+            .ho:hover{
+                background-color: ${theme.textc};
+            }
+            `}
+           </style>
+
+            <div className="flex flex-row flex-wrap justify-between  p-2" style={{ backgroundColor: theme.theme1, color: theme.headingc }}>
                 <div className="flex flex-row flex-initial flex-wrap items-center justify-center  ">
-                    <div className="mr-4">
+                    <div className="mr-4 ho p-2 rounded-md" >
                         <button>
                             <TfiMenu className="md:h-6 md:w-6 h-5 w-5" />
                         </button>
                     </div>
-                    <div className="">
+                    <div className="ho p-2 rounded-md">
                         <Link className="flex flex-row" href="/">
                             <Image
                                 src={logo}
@@ -39,30 +48,30 @@ export default function navbar() {
                             <h1 className="md:text-2xl text-xl font-bold">dezynUI</h1>
                         </Link>
                     </div>
-
                 </div>
+
                 <div className="flex flex-row flex-initial flex-wrap items-center justify-center   mx-2">
-                    <div className="hidden mx-6 flex-none items-center lg:block ">
+                    <div className="hidden mx-6 flex-none items-center lg:block ho p-2 rounded-md">
                         <Link href="/components">
                             <p>Components</p>
                         </Link>
                     </div>
-                    <div>
+                    <div className="ho p-2 rounded-md">
                         <button onClick={handleDropdownToggle}>
                             <TbColorSwatch className="md:h-6 md:w-6 h-5 w-5 md:hidden" />
                             <div className="hidden md:inline-block  item-center justify-center ">Theme▼ </div>
                         </button>
                         {isOpen && (
-                            <div className="absolute top-20 flex-col right-5  p-4 shadow-md " style={{backgroundColor: theme.theme2}}>
+                            <div className="absolute top-20 flex-col right-5  p-4 rounded-2xl" style={{ backgroundColor: theme.theme2 }}>
                                 <div className="flex flex-col gap-6 justify-items-start">
-                                    <div className="px-4 py-2" style={{backgroundColor: light.theme1, color: light.headingc}}><button onClick={() => switchTheme('light')}>Light Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: dark.theme1, color: dark.headingc}}><button onClick={() => switchTheme('dark')}>Dark Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: vintage.theme1, color:vintage.headingc}}><button onClick={() => switchTheme('vintage')}>Vintage Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: forest.theme1, color: forest.headingc}}><button onClick={() => switchTheme('forest')}>Forest Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: rose.theme1, color: rose.headingc}}><button onClick={() => switchTheme('rose')}>rose Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: ocean.theme1, color: ocean.headingc}}> <button onClick={() => switchTheme('ocean')}>Ocean Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: colorful.theme1, color: colorful.headingc}}> <button onClick={() => switchTheme('colorful')}>Colorful Theme</button></div>
-                                    <div className="px-4 py-2" style={{backgroundColor: black.theme1, color: black.headingc}}><button onClick={() => switchTheme('black')}>Black Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: light.theme1, color: light.headingc }}><button onClick={() => switchTheme('light')}>Light Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: dark.theme1, color: dark.headingc }}><button onClick={() => switchTheme('dark')}>Dark Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: vintage.theme1, color: vintage.headingc }}><button onClick={() => switchTheme('vintage')}>Vintage Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: forest.theme1, color: forest.headingc }}><button onClick={() => switchTheme('forest')}>Forest Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: rose.theme1, color: rose.headingc }}><button onClick={() => switchTheme('rose')}>Rose Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: ocean.theme1, color: ocean.headingc }}> <button onClick={() => switchTheme('ocean')}>Ocean Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: colorful.theme1, color: colorful.headingc }}> <button onClick={() => switchTheme('colorful')}>Colorful Theme</button></div>
+                                    <div className="px-4 py-2 rounded-2xl" style={{ backgroundColor: black.theme1, color: black.headingc }}><button onClick={() => switchTheme('black')}>Black Theme</button></div>
                                 </div>
 
                             </div>

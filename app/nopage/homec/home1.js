aca14a4b91780fc3cb2e16e46d2598b441489470
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-02-11 20:56:12 +0530

    updated

--- app/nopage/homec/home1.js
+++ app/nopage/homec/home1.js
@@ -6,6 +6,7 @@ import themeContext from "../context/themeContext";
 import { useContext } from "react";
 import React, { useEffect } from 'react';
 import Typed from 'typed.js';
+import Link from "next/link";
 
 export default function page() {
 
@@ -38,28 +39,44 @@ export default function page() {
                     background-clip: text;
                     color: transparent;
                 }
+                .ho:hover{
+                    background-color: ${theme.g1};
+                }
+
                 `
                 }
             </style>
+         
 
-            <div className="flex xl:flex-row  flex-wrap flex-col h-screen " style={{ backgroundColor: theme.theme1 }}>
-                <div className="flex flex-col xl:w-3/5 border-4 border-red-600 text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-black">
-                    <div className="flex flex-row  ">
-                        <h1>The most </h1><span>&nbsp;</span> <span className="typed "></span>
+            <div className="flex flex-col xl:flex-row flex-wrap xl:h-screen h-max " style={{ backgroundColor: theme.theme1 }}>
+                <div className="flex flex-col xl:w-3/5 xl:mt-12 xl:p-12 p-6">
+                    <div className="flex flex-col  text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-black" style={{ color: theme.headingc }}>
+                        <div className="flex flex-row py-3">
+                            <h1>The most </h1><span>&nbsp;</span> <span className="typed"></span>
+                        </div>
+                        <div className="hg py-3 ">
+                            Ready-to-use
+                        </div>
+                        <div className="py-3 ">
+                            Tailwind CSS components
+                        </div>
                     </div>
-                    <div className="hg ">
-                        Ready-to-use
+                    <div className="py-3 my-4 text-xl" style={{ color: theme.textc }}>
+                        <p>Explore our ever-growing library and see how it can take your projects to the next level  </p>
+                        
                     </div>
-                    <div className="">
-                        Tailwind CSS components
+                    <div className="ho py-3 xl:mt-9 flex w-max p-2 px-6 rounded-md" style={{ backgroundColor: theme.theme2, color: theme.headingc }}>
+                        <Link href="/components">
+                            <p>Explore Components</p>
+                        </Link>
                     </div>
+
                 </div>
-                <div className=" xl:w-2/5  " >
-
-                    <MySvg className=" w-full " />
-
+                <div className="w-full xl:w-2/5 xl:mt-12 ">
+                    <MySvg className="w-full" />
                 </div>
             </div>
+
 
         </>
     );

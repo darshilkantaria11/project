c798548d44ae79ae6b1a117f8b86099fd7df4bd8
darshilkantaria11 <darshilkantaria11@gmail.com>
2024-02-11 21:59:51 +0530

    updated

--- app/nopage/homec/home1.js
+++ app/nopage/homec/home1.js
@@ -34,28 +34,29 @@ export default function page() {
             <style jsx>
                 {
                     `
-                .hg{
-                    background: linear-gradient(90deg, ${theme.g2} 0%, ${theme.g1} 25%, ${theme.g3} 40% );
-                    background-clip: text;
-                    color: transparent;
-                }
-                .ho:hover{
-                    background-color: ${theme.g1};
-                }
+        .hg {
+            background: linear-gradient(90deg, ${theme.g2} 0%, ${theme.g1} 25%, ${theme.g3} 40%);
+            background-clip: text;
+            color: transparent;
+        }
 
-                `
+       
+        
+        
+        `
                 }
             </style>
-         
+
+
 
             <div className="flex flex-col xl:flex-row flex-wrap xl:h-screen h-max " style={{ backgroundColor: theme.theme1 }}>
                 <div className="flex flex-col xl:w-3/5 xl:mt-12 xl:p-12 p-6">
-                    <div className="flex flex-col  text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-black" style={{ color: theme.headingc }}>
-                        <div className="flex flex-row py-3">
+                    <div className="flex flex-col  text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-black " style={{ color: theme.headingc }}>
+                        <div className="flex flex-row py-3 ">
                             <h1>The most </h1><span>&nbsp;</span> <span className="typed"></span>
                         </div>
-                        <div className="hg py-3 ">
-                            Ready-to-use
+                        <div className="hg  py-3 ">
+                            <h1>Ready-to-use</h1>
                         </div>
                         <div className="py-3 ">
                             Tailwind CSS components
@@ -63,9 +64,9 @@ export default function page() {
                     </div>
                     <div className="py-3 my-4 text-xl" style={{ color: theme.textc }}>
                         <p>Explore our ever-growing library and see how it can take your projects to the next level  </p>
-                        
+
                     </div>
-                    <div className="ho py-3 xl:mt-9 flex w-max p-2 px-6 rounded-md" style={{ backgroundColor: theme.theme2, color: theme.headingc }}>
+                    <div className="ho py-3 xl:mt-9 flex w-max p-2 px-6 rounded-md decoration-4 " style={{ backgroundColor: theme.theme2, color: theme.headingc }}>
                         <Link href="/components">
                             <p>Explore Components</p>
                         </Link>
